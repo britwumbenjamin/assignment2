fe3412542b6d4e54a8b9d903ea2aaea42eff365e
Benjamin Britwum <britwumbenjamin>
2021-05-29 15:34:28 +0000

    two

---
+++ App.js
@@ -0,0 +1,36 @@
+
+import MyFirstComponent from './components/MyFirstComponent'
+import MySecondComponent from './components/MySecondComponent';
+import MyThirdComponent from './components/MyThirdComponent';
+import MyFourthComponent from './components/MyFourthComponent';
+import MyFifthComponent from './components/MyFifthComponent';
+import MySixthComponent from './components/MySixthComponent';
+import MySeventhComponent from './components/MySeventhComponent';
+import MyEightComponent from './components/MyEightComponent';
+import MyNinthComponent from './components/MyNinthComponent';
+import MyTenthComponent from './components/MyTenthComponent';
+
+
+
+
+function App() {
+
+  return (
+    <div>
+      <MyFirstComponent/>
+      <MySecondComponent/>
+      <MyThirdComponent/>
+      <MyFourthComponent/>
+      <MyFifthComponent/>
+      <MySixthComponent/>
+      <MySeventhComponent/>
+      <MyEightComponent/>
+      <MyNinthComponent/>
+      <MyTenthComponent/>
+
+    </div>
+
+  );
+}
+
+export default App;
